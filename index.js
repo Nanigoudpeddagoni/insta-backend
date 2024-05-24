42d04e2d6467c9c5432dc2518202bbf033ff1dbd
nani goud <nanigoudpeddagoni@gmail.com>
2024-05-24 21:55:16 +0530

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const bodyParser = require("body-parser")
 const path = require('path')
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://insta-frontend-rho.vercel.app'
 }));
 app.use("/auth", signup)
 app.use("/auth", login)
